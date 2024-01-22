4b507196218d713549e948356846e55fad776e00
daitasu <ifctp.dai.monara@gmail.com>
2024-01-22 23:33:47 +0900

    feat: Headerを追加

--- packages/flying-shigechi/nuxt.config.ts
+++ packages/flying-shigechi/nuxt.config.ts
@@ -2,4 +2,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  app: {
+    head: {
+      title: "Flying Shigechi",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "空を飛ぶ Shigechi がキノコを追い求める物語です",
+        },
+      ],
+    },
+  },
 });
---
+++ packages/flying-shigechi/tailwind.config.js
@@ -0,0 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    screens: {
+      pc: { min: "580px" },
+    },
+    fontFamily: {
+      sans: [
+        "IBM Plex Sans",
+        "ヒラギノ角ゴシック",
+        "Hiragino Sans",
+        "Yu Gothic UI",
+        "Yu Gothic",
+        "メイリオ",
+        "Meiryo",
+        "sans-serif",
+      ],
+    },
+    fontWeight: {
+      normal: 300,
+      bold: 600,
+    },
+    fontSize: {
+      16: ["16px", { lineHeight: "1.6", letterSpacing: "0.02em" }],
+      20: ["20px", { lineHeight: "1.6", letterSpacing: "0.02em" }],
+      24: ["24px", { lineHeight: "1.6", letterSpacing: "0.02em" }],
+      32: ["32px", { lineHeight: "1.6", letterSpacing: "0.02em" }],
+    },
+    boxShadow: {
+      DEFAULT: "0 8px 16px rgba(0, 0, 0, 0.12)",
+    },
+    spacing: {
+      0: "0px",
+      0.5: "4px",
+      1: "8px",
+      1.5: "12px",
+      2: "16px",
+      2.5: "20px",
+      3: "24px",
+      4: "32px",
+      5: "40px",
+      6: "48px",
+      7: "56px",
+      8: "64px",
+      9: "72px",
+    },
+    borderRadius: {
+      0: "0px",
+      4: "4px",
+      8: "8px",
+      12: "12px",
+      16: "16px",
+      20: "20px",
+      full: "9999px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
