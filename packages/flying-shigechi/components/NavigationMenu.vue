401ac84f1115cf206168ce8f4153e0dcf32900e2
daitasu <ifctp.dai.monara@gmail.com>
2024-01-23 23:25:08 +0900

    feat: Menu の開閉アニメーションを追加

--- packages/flying-shigechi/components/NavigationMenu.vue
+++ packages/flying-shigechi/components/NavigationMenu.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-const route = useRoute();
-
 type Props = {
   isOpen: boolean;
 };
 
 const props = defineProps<Props>();
 
+const route = useRoute();
+
 const emit = defineEmits<{
-  (e: "onClick", value: string): void;
+  (e: "onClickClose"): void;
 }>();
 
 const menus = [
@@ -27,34 +27,74 @@ const menus = [
 const isActiveMenu = (to: string) => {
   return route.path === to;
 };
+
+const handleClose = () => {
+  emit("onClickClose");
+};
 </script>
 
 <template>
-  <div
-    v-if="isOpen"
-    :class="{ 'navigation--active': props.isOpen }"
-    class="navigation fixed top-0 left-0 h-full w-300 text-gray-600 bg-white py-1 px-2 bold text-20"
-  >
-    <NuxtLink
-      v-for="menu in menus"
-      :to="menu.to"
-      :class="{ 'text-blue-600': isActiveMenu(menu.to) }"
-      class="h-48 px-1 flex flex-row items-center hover:opacity-70 hover:bg-gray-200 transition"
-    >
-      <Icon :name="menu.icon" color="text-gray-600" size="24" />
-      <p class="ml-2">{{ menu.title }}</p>
-    </NuxtLink>
+  <div>
+    <div
+      v-if="isOpen"
+      class="overlay fixed top-0 right-0 left-0 bottom-0 transition opacity-50"
+      @click="handleClose"
+    />
+    <Transition appear>
+      <div
+        v-if="isOpen"
+        class="navigation fixed top-0 left-0 h-full w-300 text-gray-600 bg-white py-1 px-2 bold text-20"
+      >
+        <NuxtLink
+          v-for="menu in menus"
+          :to="menu.to"
+          :class="{ 'text-blue-600': isActiveMenu(menu.to) }"
+          class="h-48 px-1 flex flex-row items-center hover:opacity-80 hover:bg-blue-100 transition"
+          @click="handleClose"
+        >
+          <Icon :name="menu.icon" color="text-gray-600" size="24" />
+          <p class="ml-2">{{ menu.title }}</p>
+        </NuxtLink>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <style lang="scss">
 .navigation {
-  transform: translateX(-300px);
+  z-index: 3;
   box-shadow: 0 8px 10px -5px rgba(0, 0, 0, 0.2),
     0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12);
 
-  &--active {
-    transform: translateX(0px);
+  transition-property: transform, opacity;
+  transition-duration: 0.2s;
+}
+.overlay {
+  z-index: 2;
+
+  &:before {
+    background-color: #212121;
+    bottom: 0;
+    content: "";
+    height: 100%;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transition: inherit;
+    transition-delay: 0.15s;
+    width: 100%;
   }
+}
+
+.v-enter-active,
+.v-leave-active {
+  opacity: 1;
+  transform: translateX(0);
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-300px);
 }
 </style>
--- packages/flying-shigechi/layouts/default.vue
+++ packages/flying-shigechi/layouts/default.vue
@@ -3,14 +3,12 @@ import { ref } from "vue";
 import NavigationMenu from "~/components/NavigationMenu.vue";
 
 const isOpen = ref<boolean>(false);
-const openMenu = () => (isOpen.value = !isOpen.value);
+const openMenu = () => (isOpen.value = true);
+const closeMenu = () => (isOpen.value = false);
 </script>
 
 <template>
   <div class="text-16">
-    <div
-      class="overlay fixed top-0 right-0 left-0 bottom-0 pointer-events-none transition opacity-0"
-    />
     <header>
       <div
         class="header fixed top-0 right-0 w-full flex justify-between items-center bg-blue-200"
@@ -27,7 +25,7 @@ const openMenu = () => (isOpen.value = !isOpen.value);
         </a>
       </div>
     </header>
-    <NavigationMenu :isOpen="isOpen" />
+    <NavigationMenu :isOpen="isOpen" @onClickClose="closeMenu" />
     <slot />
   </div>
 </template>
@@ -38,22 +36,5 @@ const openMenu = () => (isOpen.value = !isOpen.value);
   height: 56px;
   box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.14),
     0 1px 10px 0 rgba(0, 0, 0, 0.12);
-}
-.overlay {
-  z-index: 2;
-
-  &:before {
-    background-color: #212121;
-    bottom: 0;
-    content: "";
-    height: 100%;
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 0;
-    transition: inherit;
-    transition-delay: 0.15s;
-    width: 100%;
-  }
 }
 </style>
