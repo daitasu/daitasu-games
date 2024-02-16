b6c47454a011b07dd9fc1443f3ca36c95922fdeb
daitasu <ifctp.dai.monara@gmail.com>
2024-02-16 12:13:16 +0900

    feat: リリース

--- packages/flying-shigechi/nuxt.config.ts
+++ packages/flying-shigechi/nuxt.config.ts
@@ -17,9 +17,25 @@ export default defineNuxtConfig({
         {
           hid: "description",
           name: "description",
-          content: "空を飛ぶ Shigechi がキノコを追い求める物語です",
+          content: "空を飛ぶ Shigechi が野菜と共にキノコを追い求める物語です",
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "website",
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://flying-shigechi.web.app/",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "/ogp.png",
         },
       ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
 });
